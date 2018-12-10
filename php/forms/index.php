c2b8b778b41bbdafa795e9c00375588cfa0493f1
Kalpins <rolands.kalpins@gmail.com>
2018-12-10 16:40:25 +0200

    nomainīts dokumenta title

--- php/forms/index.php
+++ php/forms/index.php
@@ -31,7 +31,7 @@ if (isset($_POST['submit'])){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Documents</title>
 </head>
 <body>
     <form method="post" action="index.php">
